9cb0cb9598e5a8595154dd19e126274c1a187962
MacOS <macos@MacOSdeMacBook-Pro.local>
2025-09-17 13:51:11 +0800

    新增logo图片

--- jeecgboot-vue3/src/views/dashboard/workbench/components/AiOpsLinkCard.vue
+++ jeecgboot-vue3/src/views/dashboard/workbench/components/AiOpsLinkCard.vue
@@ -26,7 +26,7 @@
 .nav-grid {
   display: flex;
   flex-wrap: wrap;
-  gap: 5px 5px; /* 行间距48，列间距64，接近示意图 */
+  gap: 17px 17px; /* 行间距48，列间距64，接近示意图 */
   padding: 10px 8px 8px;
 }
 .nav-item {
@@ -63,14 +63,32 @@ export default defineComponent({
   components: { Card },
   setup() {
     const defaultIcon = '/resource/img/logo.png';
+    const aiopsIcon = '/resource/img/AIOPS.jpeg';
+    const cmdbIcon = '/resource/img/CMDB.jpeg';
+    const itilIcon = '/resource/img/ITIL.jpeg';
+    const hundunIcon = '/resource/img/混沌工程.jpeg';
+    const riyijyIcon = '/resource/img/日志易.jpeg';
+    const jizhongjiankongIcon = '/resource/img/集中监控.jpeg';
+    const liuliangfenxiIcon = '/resource/img/流量分析.jpeg';
+    const quanxianyinheIcon = '/resource/img/权限稽核.jpeg';
+    const zcloudIcon = '/resource/img/ZCLOUD.jpeg';
+    const kelaIcon = '/resource/img/科来.jpeg';
+    const hengweiIcon = '/resource/img/恒维.jpeg';
+
+
+
     const apps = ref<AppLink[]>([
-      { name: 'AIOPS', url: 'https://your-itil.example.com',icon: defaultIcon },
-      { name: 'OA', url: 'https://your-cloud-disk.example.com' },
-      { name: '混沌工程', url: 'https://mail.example.com' },
-      { name: '日志易', url: 'https://oa.example.com' },
-      { name: '集中监控', url: 'https://hr.example.com' },
-      { name: '流量分析', url: 'https://cmdb.example.com' },
-      { name: 'CMDB', url: 'https://cmdb.example.com' },
+      { name: 'AIOPS', url: 'http://10.56.190.109:8080/authentication/login',icon: aiopsIcon},
+      { name: 'ITIL', url: 'http://10.56.190.193/',icon: itilIcon},
+      { name: '混沌工程', url: 'https://10.56.82.73/#/login?redirect=%2Fdashboard',icon: hundunIcon},
+      { name: '日志易', url: 'http://10.55.43.40/auth/login/',icon: riyijyIcon},
+      { name: '集中监控', url: 'http://10.55.59.70:8081/alert/active',icon: jizhongjiankongIcon},
+      { name: '流量分析', url: 'http://10.55.59.50:8089/#/',icon: liuliangfenxiIcon},
+      { name: 'CMDB', url: 'http://10.55.74.108/next/next-cmdb-instance-management/discard', icon: cmdbIcon},
+      { name: '权限稽核', url: 'http://10.55.188.69:8080/login',icon: quanxianyinheIcon},
+      { name: 'ZCLOUD', url: 'http://10.55.59.152:8080/enmoLogin',icon: zcloudIcon}, 
+      { name: '科来', url: 'https://10.56.83.56/login.html',icon: kelaIcon},
+      { name: '恒维', url: 'http://10.56.83.5/hengwei/sessions/login',icon: hengweiIcon},
     ]);
 
     function open(url: string) {
